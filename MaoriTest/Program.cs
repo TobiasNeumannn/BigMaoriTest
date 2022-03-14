f6fbb63702e945ed573d58ccd03bf9fa1fa35891
Tobi Neumann <ac117312@avcol.school.nz>
2022-03-15 11:11:11 +1300

    Added CleanUp Method.

--- MaoriTest/Program.cs
+++ MaoriTest/Program.cs
@@ -8,25 +8,26 @@ public class Program
 		Console.WriteLine("Welcome to the Grand Epic Cool Style C# Maori Test!!! Created by Tobias Neumann. \n");
 		Random rnd = new Random();
 		int score = 0;
+		int index;
 
 		// database of maori words and english meanings
-		string[] array1 = { "iwi", "kai", "haere mai", "wahine", "e tu", "e noho", "whanua", "kia ora", "turituri", "morena" };
-		string[] array2 = { "tribe", "food", "come here", "woman", "stand up", "sit down", "family", "hello", "shut up", "good morning" };
+		string[] maoriWords = { "iwi", "kai", "haere mai", "wahine", "e tu", "e noho", "whanua", "kia ora", "turituri", "morena" };
+		string[] english = { "tribe", "food", "come here", "woman", "stand up", "sit down", "family", "hello", "shut up", "good morning" };
 
 		// actual quiz, asking questions
-		foreach (string element in array1)
+		foreach (string element in maoriWords)
 		{
 			// change text color of question back to white
 			Console.ForegroundColor = ConsoleColor.White;
-			int index = rnd.Next(10);
+			index = rnd.Next(10);
 			Console.Write("What is the definition of: ");
-			Console.WriteLine(array1[index]);
+			Console.WriteLine(maoriWords[index]);
 			string answer = Console.ReadLine();
 			// a gap to look good
 			Console.WriteLine("");
 
 			//checks if answer is correct
-			if (answer.ToLower() == array2[index])
+			if (CleanUp(answer) == english[index])
             {
 				// change text color
 				Console.ForegroundColor = ConsoleColor.Green;
@@ -39,5 +40,15 @@ public class Program
 				Console.WriteLine("Incorrect! \n");
             }
 		}
+
+	}
+	// cleans up the answer: if the user inputs capitals in the answer, or adds a space after the answer, this method will correct it so program reads answer as correct
+	public static string CleanUp(string answer)
+    {
+		string answer1 = answer.ToLower();
+		int length = (english[index]).Length;
+		var replacement = answer1.Replace(" ", "");
+		return replacement;
+
 	}
 }
