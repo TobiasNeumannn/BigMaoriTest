d400ef02bcb2d2d806ee054c9c373ec8f5ea4d43
Tobi Neumann <ac117312@avcol.school.nz>
2022-05-04 10:11:01 +1200

    Added picking level system.

--- MaoriTest/Program.cs
+++ MaoriTest/Program.cs
@@ -37,75 +37,103 @@ inside Run() method -
 	index increases by 1
 */
 
-Console.ForegroundColor = ConsoleColor.Blue;
-Console.WriteLine("Welcome to the Grand C# Maori Test!!! Created by Tobias Neumann. \n");
-Console.WriteLine("Level 1");
-Console.WriteLine("-------");
-
-int score = 0; // num of questions answered correctly
-int index1 = 0; // place of maori/english word in the array
-
 // array of maori words and english meanings. every maori word has the same index/place of its english meaning
 string[] maoriWords = { "iwi", "kai", "haere mai", "wahine", "e tu", "e noho", "whanau", "kia ora", "turituri", "morena" };
 string[] english = { "tribe", "food", "come here", "woman", "stand up", "sit down", "family", "hello", "shut up", "good morning" };
 
-// actual quiz (level 1), user types answers
-foreach (string word in maoriWords)
-{
-	// change text color of question to white
-	Console.ForegroundColor = ConsoleColor.White;
-	Console.Write(index1 + 1 + " - What is the maori word for: ");
-	Console.WriteLine(english[index1]); // fetches the english word from the array, corresponding to the current index
-	string answer = Console.ReadLine();
-
-	//checks if answer is correct
-	if ((answer.ToLower()) == maoriWords[index1]) // answer.ToLower() coverts the answer to lowercase so if user inputs capitals in their answer ("mAoRiWoRd") program still reads it as correct
-	{
-		// change text color
-		Console.ForegroundColor = ConsoleColor.Green;
-		Console.WriteLine("\nCorrect! \n");
-		score++;
-	}
-	else
-	{
-		Console.ForegroundColor = ConsoleColor.Red;
-		Console.WriteLine("\nIncorrect! The correct answer was " + maoriWords[index1] + ".\n");
-	}
-	index1++;
-}
-
-Console.ForegroundColor = ConsoleColor.Green;
-Console.Write("Congratulations! You finished level 1! your score was " + score + "/10. \n \n");
-
-// LEVEL 2: multichoice
-
 // array for Level 2 words
-string[] maoriWords2 = {"kaumatua", "kauri", "koha", "aroha", "awa", "haka", "hui", "karakia", "mahi", "mana" };
-string[] english2 = {"elder", "large native tree", "gift", "love", "river", "maori dance", "gathering/meeting", "prayer", "work", "prestige/reputation" };
-
-Console.ForegroundColor = ConsoleColor.Blue;
-Console.WriteLine("Level 2: Input the number corresponding to the correct answer");
-Console.WriteLine("-------");
-Console.ForegroundColor = ConsoleColor.White;
+string[] maoriWords2 = { "kaumatua", "kauri", "koha", "aroha", "awa", "haka", "hui", "karakia", "mahi", "mana" };
+string[] english2 = { "elder", "large native tree", "gift", "love", "river", "maori dance", "gathering/meeting", "prayer", "work", "prestige/reputation" };
 
 int answer2;
 int score2 = 0; // score for just level 2
 
-// 10 questions
-Run(0, 9, 8, 1);
-Run(1, 7, 6, 3);
-Run(2, 4, 5, 3);
-Run(3, 0, 9, 2);
-Run(4, 8, 5, 3);
-Run(5, 7, 0, 1);
-Run(6, 4, 1, 2);
-Run(7, 8, 9, 2);
-Run(8, 5, 3, 1);
-Run(9, 7, 5, 3);
+int score = 0; // num of questions answered correctly in total quiz
+int index1 = 0; // place of maori/english word in the array
 
 Console.ForegroundColor = ConsoleColor.Green;
-Console.Write("Congratulations! You finished level 2! your score for level 2 was " + score2 + "/10 and your total score is " + score + "/20. \n");
-Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("Welcome to the Grand C# Maori Test!!! Created by Tobias Neumann. \n");
+
+while (true)
+{
+	Console.WriteLine("Would you like to do Level 1 (translating English to Maori) or Level 2 (translating Maori to English?) or quit?");
+	Console.WriteLine("(Please enter '1' for Level 1 or '2' for Level 2 or any other number to quit.)");
+
+	int level;
+	while (!int.TryParse(Console.ReadLine(), out level) || level < 1 || level > 3) // checks if answer is valid - if answer isnt an integer, user must input another answer
+	{
+		Console.ForegroundColor = ConsoleColor.Red;
+		Console.Write("\nPlease enter a valid answer: ");
+	}
+	Console.ForegroundColor = ConsoleColor.White;
+
+	if (level == 1)
+	{
+		score = 0; // resets score
+
+		Console.WriteLine("Level 1");
+		Console.WriteLine("-------");
+
+		// actual quiz (level 1), user types answers
+		foreach (string word in maoriWords)
+		{
+			// change text color of question to white
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.Write(index1 + 1 + " - What is the maori word for: ");
+			Console.WriteLine(english[index1]); // fetches the english word from the array, corresponding to the current index
+			string answer = Console.ReadLine();
+
+			//checks if answer is correct
+			if ((answer.ToLower()) == maoriWords[index1]) // answer.ToLower() coverts the answer to lowercase so if user inputs capitals in their answer ("mAoRiWoRd") program still reads it as correct
+			{
+				// change text color
+				Console.ForegroundColor = ConsoleColor.Green;
+				Console.WriteLine("\nCorrect! \n");
+				score++;
+			}
+			else
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("\nIncorrect! The correct answer was " + maoriWords[index1] + ".\n");
+			}
+			index1++;
+		}
+
+		Console.ForegroundColor = ConsoleColor.Green;
+		Console.Write("Congratulations! You finished level 1! your score was " + score + "/10. \n \n");
+	}
+	else if (level == 2)
+	{
+		// LEVEL 2: multichoice
+
+		score2 = 0; // resets score2
+
+		Console.ForegroundColor = ConsoleColor.Green;
+		Console.WriteLine("Level 2: Input the number corresponding to the correct answer");
+		Console.WriteLine("-------");
+		Console.ForegroundColor = ConsoleColor.White;
+
+		// 10 questions
+		Run(0, 9, 8, 1);
+		Run(1, 7, 6, 3);
+		Run(2, 4, 5, 3);
+		Run(3, 0, 9, 2);
+		Run(4, 8, 5, 3);
+		Run(5, 7, 0, 1);
+		Run(6, 4, 1, 2);
+		Run(7, 8, 9, 2);
+		Run(8, 5, 3, 1);
+		Run(9, 7, 5, 3);
+
+		Console.ForegroundColor = ConsoleColor.Green;
+		Console.Write("Congratulations! You finished level 2! your score for level 2 was " + score2 + "/10 and your total score is " + score + "/20. \n");
+		Console.ForegroundColor = ConsoleColor.White;
+	}
+	else
+	{
+		Environment.Exit(0);
+	}
+}
 
 void Run(int index, int x, int y, int realAnswer) // one question - index is the place of the word in the array, x and y are numbers chosen randomly, and realAnswer is the place of the real answer
 {
